f445d0274513360e07dd55a49039abf1a7ebd032
LeonardoHui <96031609+LeonardoHui@users.noreply.github.com>
2022-08-01 23:20:35 -0300

    test: add more unit test for GetAllBooks handler (#2)

--- pkg/handlers/GetAllBooks_test.go
+++ pkg/handlers/GetAllBooks_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -57,5 +58,45 @@ func Test_GetAllBooks_returnsSuccessfuly(t *testing.T) {
 	body, err := ioutil.ReadAll(rr.Body)
 	expectedBody := `[{"id":1,"title":"Happy Pot","author":"Myself","description":"Parody"}]`
 	assert.Equal(t, expectedBody, string(body[:len(body)-1])) //Removing new line
+
+}
+
+func Test_GetAllBooks_returnsEmptyBook(t *testing.T) {
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	dialector := postgres.New(postgres.Config{
+		DSN:                  "sqlmock_db_0",
+		DriverName:           "postgres",
+		Conn:                 db,
+		PreferSimpleProtocol: true,
+	})
+
+	gromDb, err := gorm.Open(dialector, &gorm.Config{})
+	if err != nil {
+		t.Fatalf("[gorm open] %s", err)
+	}
+
+	h := handler{DB: gromDb}
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "books"`)).WillReturnError(errors.New("Error expected"))
+
+	req, err := http.NewRequest("GET", "/books", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	httpHandler := http.HandlerFunc(h.GetAllBooks)
+	httpHandler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+
+	body, err := ioutil.ReadAll(rr.Body)
+	assert.Equal(t, "null\n", string(body)) //Removing new line
 
 }
